53a954418a5c74d8c4982b86c523fe3f4f2fc35f
greatjing <greatjing@sina.cn>
2017-03-28 23:17:11 +0800

    add calidation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,15 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+
+    #@group.save
+    #redirect_to groups_path
   end
 
   def update
